0a3e135fa42647493cebc47fabfcee44180ec53c
ltavis <ltavis@AH-LTAVIS.dhcp.mathworks.com>
2014-02-03 14:47:50 -0500

    testing commit

--- checkout.m
+++ checkout.m
@@ -5,6 +5,7 @@ function checkout(fileNames, varargin)
 %   can be a cell array of files. By default, all files are 
 %   locked upon checkout.
 %
+%this will soon be deprecated
 %   CHECKOUT(FILENAME, OPTION1, VALUE1, OPTION2, VALUE2, ...)
 %
 %   OPTIONS can be:
